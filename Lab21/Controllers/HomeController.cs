336564762636f2e9dd4474c65f4c278077101ac6
Lovejoy-gitsit <26442317+Lovejoy-gitsit@users.noreply.github.com>
2018-05-22 17:58:48 -0400

    first commit

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult UserRegistration()
+        {
+            string[] UserDetails = { "First Name", "Last Name", "Email", "Phone Number", "Password"};
+            ViewBag.UserDetails = User;
+            return View();
+        }
+
+        public ActionResult Add(string firstname, string lastname, string email, string phonenumber, string password)
+        {
+            ViewBag.Message = firstname;
+            return View ("User");
+        }
+        
+        
+
+    }
+}
